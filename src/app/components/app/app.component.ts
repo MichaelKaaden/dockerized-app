dcf49eabb5baa356c8bfd96fde3addc1cfb1df52
Michael Kaaden <github@kaaden.net>
2019-03-11 22:11:58 +0100

    Add the concept of "settings"

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from "@angular/core";
+import { Settings } from "../../models/settings";
+import { SettingsService } from "../../services/settings.service";
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = "dockerized-app";
+    title = "dockerized-app";
+    settings: Settings;
+
+    constructor(private settingsService: SettingsService) {
+        this.settings = settingsService.settings;
+    }
 }
